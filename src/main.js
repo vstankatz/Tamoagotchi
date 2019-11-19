8ceea1587ff8d39de81df64f1ad3d9557e0810c0
Student <example@example.com>
2019-11-19 11:57:20 -0800

    add api

--- src/main.js
+++ src/main.js
@@ -5,6 +5,15 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
+function hideButtons() {
+
+  $(".buttons").hide();
+  let buttonTimer = setInterval(() => {
+    $(".buttons").show();
+    clearInterval(buttonTimer);
+  }, 5000);
+}
+
 
 
 $(document).ready(function() {
@@ -20,20 +29,46 @@ $(document).ready(function() {
     $('#species').text(health.species);
     console.log(health.healthLevel);
     console.log(health.mess);
+    console.log(process.env.API_KEY);
+    let request = new XMLHttpRequest();
+    const url = `https://api.giphy.com/v1/gifs/${"sleeping" + health.species}?api_key=${process.env.API_KEY}`;
+    let sleepingwoodelf = "3ofT5Em3FLklVV7Wvu"
+
+    request.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+      let response = JSON.parse(this.responseText);
+      getElements(response);
+}
+    };
+
+    request.open("GET", url, true);
+    request.send();
+
+    const getElements = function(response) {
+
+      $('#face').attr("src", response.data.images.original.url);
+
+    }
+
     $("button#feed").click(function() {
       health.feed();
+      hideButtons();
     });
     $("button#love").click(function() {
       health.affection();
+      hideButtons();
     });
     $("button#walk").click(function() {
       health.walk();
+      hideButtons();
     });
     $("button#sleep").click(function() {
       health.sleep();
+      hideButtons();
     });
     $("button#clean").click(function() {
       health.clean();
+      hideButtons();
     });
     $('.userInput').hide();
     $('.tamagotchi').show();
